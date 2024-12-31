9d8e5dfbaf00569ed6af542beb1e4612520a631e
Janaina Cavallari Benatti <jana.c.benatti@gmail.com>
2024-12-31 00:53:40 -0300

    Inclusão da consulta listagem para itens cadastrados

--- LeiloesTDSat/src/ProdutosDAO.java
+++ LeiloesTDSat/src/ProdutosDAO.java
@@ -16,7 +16,6 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class ProdutosDAO {
-    
 
     public void cadastrarProduto(ProdutosDTO produto) throws SQLException {
 
@@ -29,7 +28,7 @@ public class ProdutosDAO {
 
             String query = "INSERT INTO produtos (nome, valor, status) VALUES (?, ?, ?)";
             preparedStatement = conn.prepareStatement(query);
-            
+
             preparedStatement.setString(1, produto.getNome());
             preparedStatement.setInt(2, produto.getValor());
             preparedStatement.setString(3, produto.getStatus());
@@ -49,9 +48,39 @@ public class ProdutosDAO {
         //conn = new conectaDAO().connectDB();
     }
 
-    public ArrayList<ProdutosDTO> listarProdutos() {
-         ArrayList<ProdutosDTO> listagem = new ArrayList<>();
-        return listagem;
+    public ArrayList<ProdutosDTO> listarProdutos() throws SQLException {
+    ArrayList<ProdutosDTO> listagem = new ArrayList<>();
+    Connection conn = null;
+    PreparedStatement preparedStatement = null;
+
+    try {
+        conectaDAO conexao = new conectaDAO();
+        conn = conexao.connectDB();
+
+        String sql = "SELECT * FROM produtos";
+        preparedStatement = conn.prepareStatement(sql);
+
+        ResultSet resposta = preparedStatement.executeQuery();
+        while (resposta.next()) {
+            ProdutosDTO p = new ProdutosDTO();
+            p.setId(resposta.getInt("id"));
+            p.setNome(resposta.getString("nome"));
+            p.setValor(resposta.getInt("valor")); 
+            p.setStatus(resposta.getString("status"));
+            listagem.add(p);
+        }
+    } catch (SQLException e) {
+        e.printStackTrace();
+        throw e;
+    } finally {
+        if (preparedStatement != null) {
+            preparedStatement.close();
+        }
+        if (conn != null) {
+            conn.close();
+        }
     }
- 
+
+    return listagem; 
+}
 }
